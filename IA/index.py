a583dcc59b17b326521f8ba227eb0b91fbf8f3a1
Nathan Santos Ataliba <nathan.s@inatel.br>
2024-03-07 10:04:58 -0300

    Google IA example :construction:

---
+++ IA/index.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+import textwrap
+import google.generativeai as genai
+from IPython.display import display
+from IPython.display import Markdown
+
+load_dotenv()
+GOOGLE_API_KEY=os.environ.get('GOOGLE_API_KEY')
+genai.configure(
+  api_key=GOOGLE_API_KEY
+)
+
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+model = genai.GenerativeModel('gemini-pro')
+response = model.generate_content("What is the meaning of life?")
+print(response)
