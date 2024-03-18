fa0b7302927a754c0f0194511276c1dc2be75e74
Nathan Santos Ataliba <nathan.s@inatel.br>
2024-03-18 12:01:56 -0300

    remove archives and add API and IA training

--- IA/treinamento.py
+++ IA/treinamento.py
@@ -1,102 +1,66 @@
-import torch
-from torch.utils.data import Dataset, DataLoader
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, AdamW
-from torch.optim import AdamW as TorchAdamW
-from pymongo import MongoClient 
+from flask import Flask, request, jsonify
+from pymongo import MongoClient
+import datetime
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
-# Exemplo de uma classe de conjunto de dados personalizada
-class QADataset(Dataset):
-    def __init__(self, data, max_seq_length=512):
-        self.data = data
-        self.max_seq_length = max_seq_length
-        self.tokenizer = AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+# isso aqui só precisa para corrigir o bug
+from spacy.cli import download
 
-    def __len__(self):
-        return len(self.data)
+download("en_core_web_sm")
 
-    def __getitem__(self, idx):
-        question = self.data[idx]['question']
-        answer = self.data[idx]['answer']
+class ENGSM:
+    ISO_639_1 = 'en_core_web_sm'
 
-        # Truncate ou pad as sequências para o comprimento máximo
-        inputs = self.tokenizer(question, answer, return_tensors="pt", max_length=self.max_seq_length, truncation=True, padding='max_length')
-        
-        return {
-            'input_ids': inputs['input_ids'].squeeze(),
-            'attention_mask': inputs['attention_mask'].squeeze(),
-            'start_positions': torch.tensor([0]),  # Placeholder, você precisará ajustar isso conforme necessário
-            'end_positions': torch.tensor([len(answer.split()) - 1])  # Placeholder, ajuste conforme necessário
-        }
+chatbot = ChatBot("BotLira",tagger_language=ENGSM)
+client = MongoClient('localhost', 27017)
+db = client['chatbot_db']
+collection = db['database']
 
-# Conectar ao MongoDB
-client = MongoClient('mongodb://localhost:27017/')
-db = client.dados_treinamento
-collection = db.dados
+# Recuperar perguntas e respostas do MongoDB
+conversas = []
+for document in collection.find():
+    conversas.append(document['pergunta'])
+    conversas.append(document['resposta'])
 
-# Exemplo de consulta ao MongoDB
-data_from_mongo = list(collection.find({},{'_id': 0}))
+# Treinar chatbot com as perguntas e respostas recuperadas
+trainer = ListTrainer(chatbot)
+trainer.train(conversas)
 
-# Verificar se o conjunto de dados não está vazio
-if len(data_from_mongo) == 0:
-    raise ValueError("O conjunto de dados MongoDB está vazio.")
+app = Flask(__name__)
 
-# Criar um conjunto de dados
-dataset = QADataset(data_from_mongo)
-
-# Verificar se há documentos suficientes para formar um batch
-batch_size = 3
-if len(dataset) < batch_size:
-    raise ValueError("Não há documentos suficientes para formar um batch.")
-
-# Criar um DataLoader
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-
-# Carregar o modelo pré-treinado para perguntas e respostas
-model_name = "bert-large-uncased-whole-word-masking-finetuned-squad"
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name) 
-
-# Parâmetros de treinamento
-optimizer = TorchAdamW(model.parameters(), lr=1e-5)
-epochs = 3
-
-# Treinamento
-for epoch in range(epochs):
-    for batch in dataloader:
-        optimizer.zero_grad()
-        outputs = model(**batch)
-        loss = outputs.loss
-        loss.backward()
-        optimizer.step()
-
-# Salvando o modelo treinado (opcional)
-# model.save_pretrained('dados/modelo')
-# dataset.tokenizer.save_pretrained('dados/tokenizer')
-
-# Preparar uma pergunta e um contexto
-question = "Qual e a capital do Brasil?"
-context = "O Brasil é um país localizado na América do Sul, conhecido por suas vastas florestas tropicais e praias deslumbrantes."
-
-# Tokenizar a pergunta e o contexto
-inputs = dataset.tokenizer(question, context, return_tensors='pt', max_length=512, truncation=True)
-
-# Realizar Inferência
-with torch.no_grad():
-    outputs = model(**inputs)
-
-# Recuperar as posições iniciais e finais das respostas
-start_logits = outputs.start_logits
-end_logits = outputs.end_logits
-
-# Encontrar a melhor resposta com base nas posições iniciais e finais
-start_index = torch.argmax(start_logits)
-end_index = torch.argmax(end_logits)
-
-# Recuperar a resposta da tokenização original
-answer_tokens = inputs['input_ids'][0][start_index:end_index+1]
-answer = dataset.tokenizer.decode(answer_tokens, skip_special_tokens=True)
+conversation_collection = db['conversations']
 
 
-# Imprimir a resposta
-print("Pergunta:", question)
-print("Resposta Gerada pelo Modelo:", answer_tokens)
+@app.route('/perguntas', methods=['GET'])
+def get_perguntas():
+    perguntas = list(collection.find({}, {'_id': 0,'pergunta':1}))
+    print(perguntas)
+    return jsonify(perguntas)
+
+@app.route('/conversations', methods=['GET'])
+def get_conversas():
+    conversas = list(conversation_collection.find({}, {'_id': 0}))
+    print(conversas)
+    return jsonify(conversas)
+
+@app.route('/receive_message', methods=['POST'])
+def receive_message():
+    data = request.json
+    message = data['message']
+    # Aqui você processa a mensagem recebida pelo cliente e obtém a resposta do bot
+    response = process_message(message)
+    # Salva a conversa no MongoDB
+    conversation_collection.insert_one({
+        'message': message,
+        'bot_response': response,
+        'timestamp': datetime.datetime.now()
+    })
+    return response
+
+def process_message(message):
+    response = chatbot.get_response(message)
+    return str(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
