06ea24353cceaddc9ddad71825478bc31f403a9c
NathanAtaliba <nathanataliba28@gmail.com>
2024-05-31 15:30:54 -0300

    add cors

--- backend/api/main.py
+++ backend/api/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from routes.user import routes_user
 from routes.conversation import routes_conversation
 from flask_swagger_ui import get_swaggerui_blueprint
+from flask_cors import CORS
 
 swagger_url="/docs"
 api_url="/static/swagger.json"
@@ -15,6 +16,11 @@ swagger = get_swaggerui_blueprint(
         'app_name': 'API chatbot'
     }
 )
+# Permitindo solicitações de todas as origens
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Habilitar CORS para todas as rotas
+CORS(app)
 
 app.register_blueprint(swagger, url_prefix=swagger_url)
 app.register_blueprint(routes_conversation)
